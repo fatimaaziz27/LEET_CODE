d0d63b8bc41a5761a778a8c838e3014b3fadfa1b
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-07-27 21:16:29 +0500

    Update 0283-move-zeroes.java

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int insertpos = 0;
+        
         for (int i=0; i<nums.length; i++){
             if (nums[i]!=0){
                 nums[insertpos] = nums[i];
                 insertpos++;
             }
         }
+        
         while(insertpos < nums.length){
             nums[insertpos] = 0;
             insertpos++;
