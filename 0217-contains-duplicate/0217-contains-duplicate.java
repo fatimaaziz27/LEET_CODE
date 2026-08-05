3b48024f1a6b8aaf0e1082dcf420d5adf1ac97e0
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-06 02:18:16 +0500

    Time: 17 ms (72.1%), Space: 108.1 MB (32.79%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-
         HashSet<Integer> seen = new HashSet<>();
 
          for (int i = 0; i < nums.length; i++){
@@ -8,14 +7,12 @@ class Solution {
             if (seen.contains(nums[i])) {
                 return true;
             }
-
             else {
                 seen.add(nums[i]);
 
             }
-        
+            
         }
-        
         return false;
     }
 }
